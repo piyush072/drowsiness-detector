18710f08bc1163ad7ab65154907cb789c5f296e3
Piyush <2017ugcs007@nitjsr.ac.in>
2019-08-06 15:33:34 +0530

    drowsiness detection

--- eye.py
+++ eye.py
@@ -1,6 +1,7 @@
 import cv2 
 import numpy as np
 import dlib
+import time
 
 cap = cv2.VideoCapture(0) 
 
@@ -23,22 +24,27 @@ while 1:
         landmarks = predictor(gray, face)
         font = cv2.FONT_HERSHEY_SIMPLEX
         
-        try:
-            if(landmarks):
-                cv2.putText(frame,"Nikal lavde, Pehli fursat me nikal" ,(60,60), font,1,(0,244,0),2)
-        except:
-            print()
+        # try:
+        #     if(landmarks):
+        #         cv2.putText(frame,"face detected" ,(60,60), font,1,(0,244,0),2)
+        # except:
+        #     print()
 
-        # left_eye = np.array([[]],np.int32) 
-        # for i in range(36,42):
-        #     left_eye = np.append(left_eye,np.array([landmarks.part(i).x,landmarks.part(i).y]))
+        right_eye = np.array([[]],np.int32) 
+        for i in range(36,42):
+            right_eye = np.append(right_eye,np.array([landmarks.part(i).x,landmarks.part(i).y]))
+            if i==37 or i==40:
+                right_eye = np.append(right_eye, np.array([(landmarks.part(i).x+landmarks.part(i+1).x)//2,(landmarks.part(i).y+landmarks.part(i+1).y)//2]))
 
-        # right_eye = np.array([[]],np.int32)
-        # for i in range(42,48):
-        #     right_eye = np.append(right_eye,np.array([landmarks.part(i).x,landmarks.part(i).y]))
+        left_eye = np.array([[]],np.int32)
+        for i in range(42,48):
+            left_eye = np.append(left_eye,np.array([landmarks.part(i).x,landmarks.part(i).y]))
+            if i==43 or i==46:
+                left_eye = np.append(left_eye, np.array([(landmarks.part(i).x+landmarks.part(i+1).x)//2,(landmarks.part(i).y+landmarks.part(i+1).y)//2]))
 
-        # left_eye = left_eye.reshape(-1,2)
+
         # right_eye = right_eye.reshape(-1,2)
+        # left_eye = left_eye.reshape(-1,2)
 
         # outer_mouth = np.array([[]],np.int32)
         # for i in range(48,60):
@@ -48,12 +54,28 @@ while 1:
         # inner_mouth = np.array([[]],np.int32)
         # for i in range(60,68):
         #     inner_mouth = np.append(inner_mouth,np.array([landmarks.part(i).x,landmarks.part(i).y]))
-
+        blink_l = left_eye.reshape(-1,2)[6,1] - left_eye.reshape(-1,2)[3,1]
+        blink_r = right_eye.reshape(-1,2)[6,1] - right_eye.reshape(-1,2)[3,1]
+        if  blink_l <= 7 and blink_r <= 7:
+            if flag == 0:
+                tm = time.time()
+                flag = 1
+            else:
+                if time.time() - tm > 0.6:
+                    cv2.putText(frame,"Alert", (50,200),font,1,(0,0,220),2)
+            cv2.putText(frame,"Blink", (50,50),font,1,(0,220,220),2)
+            cv2.polylines(frame,[right_eye.reshape(-1,1,2)],True,(0,0,255))
+            cv2.polylines(frame,[left_eye.reshape(-1,1,2)],True,(0,0,255))
+        else:
+            flag = 0
+            tm = time.time()
+            cv2.polylines(frame,[left_eye.reshape(-1,1,2)],True,(0,255,0))
+            cv2.polylines(frame,[right_eye.reshape(-1,1,2)],True,(0,255,0))
 
         # cv2.polylines(frame,[outer_mouth.reshape(-1,1,2)],True,(0,255,0))
         # cv2.polylines(frame,[inner_mouth.reshape(-1,1,2)],True,(0,255,0))
-        # cv2.polylines(frame,[right_eye.reshape(-1,1,2)],True,(0,255,0))
-        # cv2.polylines(frame,[left_eye.reshape(-1,1,2)],True,(0,255,0))
+
+        
 
     # font = cv2.FONT_HERSHEY_SIMPLEX
     # if abs(landmarks.part(62).y - landmarks.part(66).y) > 3:
